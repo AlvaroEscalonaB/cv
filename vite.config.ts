ef2e3b43e32fe03bf246b7399628271d73f3d362
Alvaro Escalona <ajescalona@uc.cl>
2025-01-19 22:11:01 -0300

    init: Add basic setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite"
+import autoprefixer from "autoprefixer"
+import tailwindcss from "tailwindcss"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+import path from "path"
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@ui": path.resolve(__dirname, "src/ui"),
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
-});
+})
