cc3728a0e59192e41e37246bb49f5380209f576a
Alvaro Escalona <ajescalona@uc.cl>
2025-01-21 22:45:13 -0300

    main: Change default route depending on the current env

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,17 @@ import { BrowserRouter, Route, Routes } from "react-router"
 const root = document.getElementById("root")
 createRoot(root!).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router />
+  </StrictMode>,
+)
+
+export default function Router() {
+  const currentEnv = import.meta.env.DEV ? "/" : "/ss_frontend/"
+  return (
+    <BrowserRouter basename={currentEnv}>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  )
+}
