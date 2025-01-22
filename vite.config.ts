de1d63425933e1ca43d5019da6f22049262ebf1f
Alvaro Escalona <ajescalona@uc.cl>
2025-01-21 22:30:57 -0300

    main: Fix vite config and change the dist file of main workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,27 @@ import path from "path"
 import tailwindcss from "tailwindcss"
 import { defineConfig } from "vite"
 
-export default defineConfig(({ mode }) => ({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@ui": path.resolve(__dirname, "src/ui"),
+export default defineConfig(({ command }) => {
+  const config = {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+        "@components": path.resolve(__dirname, "src/components"),
+        "@ui": path.resolve(__dirname, "src/ui"),
+      },
     },
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
+      },
     },
-  },
-  plugins: [react()],
-  base: "/cv/",
-}))
+    plugins: [react()],
+    base: "/",
+  }
+
+  if (command !== "serve") {
+    config.base = "/cv/"
+  }
+
+  return config
+})
