3fe0b49537b712f2916724dff5c04e7d65e1a85a
Alvaro Escalona <escalona.alvaro.13@gmail.com>
2025-02-13 02:03:32 -0300

    refactor-content: Add professional profile

---
+++ src/components/professional_profile.tsx
@@ -0,0 +1,12 @@
+import SectionWrapper from "./section_wrapper"
+
+export default function ProfessionalProfile() {
+  return (
+    <SectionWrapper label="Perfil profesional">
+      Desarrollador de software con experiencia en el diseño y construcción de aplicaciones web, APIs y sistemas
+      desacoplados utilizando Ruby on Rails, React y FastAPI. Actualmente realizando applicaciones en Go y profundizando
+      en AWS para la creación de aplicaciones escalables y serverless. Conocimientos en servicios en la nube como Azure
+      y experiencia en process mining y análisis de datos en entornos industriales
+    </SectionWrapper>
+  )
+}
